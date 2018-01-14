773fc0ab4dac9a722ff16c7373508d565af84992
robexitus <robexitus91@web.de>
2018-01-14 15:54:26 +0100

    init

---
+++ main.py
@@ -0,0 +1,40 @@
+import numpy
+from keras.datasets import imdb
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+#from keras.layers import Dropout #or use dropout in lstm layer
+from keras.layers.convolutional import Conv1D
+from keras.layers.convolutional import MaxPooling1D
+from keras.layers.embeddings import Embedding
+from keras.preprocessing import sequence
+
+numpy.random.seed(7)
+
+top_words = 5000 #only keep 5000 most used words
+(X_train, y_train), (X_test, y_test) = imdb.load_data(num_words=top_words)
+
+max_review_length = 500
+X_train = sequence.pad_sequences(X_train, maxlen=max_review_length)
+X_test = sequence.pad_sequences(X_test, maxlen=max_review_length)
+
+embedding_vector_length = 32 #32 length vector represents each word
+
+model = Sequential()
+model.add(Embedding(top_words, embedding_vector_length, input_length=max_review_length))
+model.add(Conv1D(filters=32, kernel_size=3, padding='same', activation='relu'))
+model.add(MaxPooling1D(pool_size=2))
+model.add(LSTM(100))
+#model.add(Dropout(0.2))
+#model.add(LSTM(100, dropout=0.2, recurrent_dropout=0.2))
+#model.add(Dropout(0.2))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+print(model.summary())
+model.fit(X_train, y_train, validation_data=(X_test, y_test), epochs=3, batch_size=64)
+
+scores = model.evaluate(X_test, y_test, verbose=0)
+print("Accuracy: %.2f%%" % (scores[1]*100))
+
